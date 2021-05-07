28bd058518a9cde3940e7c3ec3a7d0649dfb6ace
JohnComo <52293046+JohnComo@users.noreply.github.com>
2021-05-06 23:23:24 -0400

    added direct message

--- bot.py
+++ bot.py
@@ -1,55 +1,111 @@
-from flask import Flask 
-from slack import WebClient # allows us to send messages and do things on our behalf
-import slackeventsapi # allows us to get events from our slack and use them
-from slackeventsapi import SlackEventAdapter
-import random
+import slack 
 import os 
-import secrets
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
 
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path = env_path)
 
 app = Flask(__name__)
 
-slack_events_adapter = SlackEventAdapter(os.environ.get(secrets.SLACK_EVENTS_TOKEN), "/slack/events", app)
+# Holds events 
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
 
-slack_web_client = WebClient(token = os.environ.get(secrets.SLACKBOT_TOKEN))
+client = slack.WebClient(token = os.environ['SLACKBOT_TOKEN'])
 
-# Template for how slack sends message
-MESSAGE_BLOCK = { 
-    "type":"section", 
-    "text": { 
-        "type":"mrkdwn", 
-        "text":""
+BOT_ID = client.api_call("auth.test")['user_id']
+
+# resets when the server is shut down 
+message_counts = {}
+welcome_messages = {}
+
+class WelcomeMessage: 
+    START_TEXT = { 
+        'type':'section', 
+        'text': { 
+            'type':'mrkdwn', 
+            'text': ( 
+                'Welcome to this awesome channel! \n\n'
+                '*Get started by completing the tasks!*'
+            )
+        }
     }
-}
 
-# These are the events handlers made by slack so there is emoji, image, etc 
-@slack_events_adapter.on("message")
+    DIVIDER = {'type': 'divider'}
+
+    def __init__(self, channel, user): 
+        self.channel = channel
+        self.user = user
+        self.icon_emoji = ':robot_face:'
+        self.timestamp = ''
+        self.completed = False
+
+    def get_message(self): 
+        return {
+            'ts': self.timestamp, 
+            'channel':self.channel, 
+            'username': 'Welcome Robot', 
+            'icon_emoji': self.icon_emoji, 
+            'blocks': [
+                self.START_TEXT, 
+                self.DIVIDER, 
+                self._get_reaction_task()
+            ]
+        }
+
+    def _get_reaction_task(self): 
+        checkmark = ':white_check_mark:'
+        if not self.completed: 
+            checkmark = ':white_large_square:'
+
+        text = f'{checkmark} *React to this message!*'
+
+        return {'type': 'section', 'text':{'type': 'mrkdwn', 'text':text}}
+
+
+def send_welcome_message(channel, user): 
+    welcome = WelcomeMessage(channel, user)
+    message = welcome.get_message()
+    response = client.chat_postMessage(**message)
+    welcome.timestamp = response['ts']
+
+    if channel not in welcome_messages:
+        welcome_messages[channel] = {}
+    welcome_messages[channel][user] = welcome
+
+    
+
+@slack_event_adapter.on('message')
 def message(payload): 
-    
-    event = payload.get("event",{})
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
 
-    text = event.get("text")
-
-    if "flip a coin" in text.lower(): 
-
-        channel_id = event.get("channel")
-
-        rand_int = random.rand_int(0,1)
-
-        if (rand_int == 0): 
-            result = "Heads"
+    # makes sure bot isnt answering itself 
+    if user_id != None and BOT_ID != user_id: 
+        if user_id in message_counts: 
+            message_counts[user_id] += 1
         else: 
-            result = "Tails"
-    
-        message = f"The result is {result}"
+            message_counts[user_id] = 1
 
-        MESSAGE_BLOCK["text"]["text"] = message
-        message_to_send = {"channel": channel_id, 
-                     "blocks": {MESSAGE_BLOCK}}
+        if text.lower() == 'start':
+            send_welcome_message(f'@{user_id}', user_id) 
 
-        return slack_web_client.chat_postMessage(**message_to_send)
+        #client.chat_postMessage(channel = channel_id, text = text)
 
-        
+# Endpoint to hold slash command 
+# Default is only GET 
+@app.route('/message-count', methods = ['POST'])
+def message_count():
+    data = request.form 
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+    client.chat_postMessage(channel = channel_id, text = 'Messages {0}'.format(message_count))
+    return Response(), 200
 
-if __name__ == '__main__': 
-    app.run(host = '0.0.0.0', port = 8080); 
+if __name__ == "__main__": 
+    app.run(debug = True, port = 4000)
